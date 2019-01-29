1b8e28c2936ffb877252fa1b7c87bc7111edce42
student <student@Veena-2.local>
2019-01-29 11:07:44 +0530

    changes to storyboard

---
+++ IntGitProj/IntGitProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntGitProj
+//
+//  Created by student on 29/01/19.
+//  Copyright © 2019 studentCreative. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
